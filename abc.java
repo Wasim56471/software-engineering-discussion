e82bc0c87e17c3a6884237f0de2b4d51c8b37e72
Wasim56471 <71776363+Wasim56471@users.noreply.github.com>
2020-09-23 19:28:55 +0430

    Create abc.java

---
+++ abc.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("the smallest number is "+smallest()+"\n");
+	}

5a4b743050d2d0b3da5eb7108cdc0065b6e585e6
Wasim56471 <71776363+Wasim56471@users.noreply.github.com>
2020-09-23 19:37:53 +0430

    Update abc.java
    more functionality are added

--- abc.java
+++ abc.java
@@ -5,3 +5,11 @@ public class Question3 {
 		// TODO Auto-generated method stub
       System.out.println("the smallest number is "+smallest()+"\n");
 	}
+	static double smallest() {
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter your first number"+"\n");
+		double x=input.nextDouble();
+		System.out.println("enter your second number"+"\n");
+		double y=input.nextDouble();
+		System.out.println("enter your third number"+"\n");
+		double z=input.nextDouble();
